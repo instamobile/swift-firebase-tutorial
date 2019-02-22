f7893bfc7b4c64807c595bf63c2cb3d417ce07fc
Florian Marcu <florian.mrc@gmail.com>
2019-02-21 21:13:31 -0800

    update gitignore

---
+++ FirebaseStarterApp/FirebaseStarterApp/Controls/ATCTextField.swift
@@ -0,0 +1,21 @@
+//
+//  ATCTextField.swift
+//  FirebaseStarterApp
+//
+//  Created by Florian Marcu on 2/21/19.
+//  Copyright © 2019 Instamobile. All rights reserved.
+//
+
+import UIKit
+
+class ATCTextField: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
---
+++ FirebaseStarterApp/FirebaseStarterApp/Controls/UITextField.swift
@@ -0,0 +1,21 @@
+//
+//  UITextField.swift
+//  FirebaseStarterApp
+//
+//  Created by Florian Marcu on 2/21/19.
+//  Copyright © 2019 Instamobile. All rights reserved.
+//
+
+import UIKit
+
+class UITextField: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
